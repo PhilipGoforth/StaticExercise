6f4ecedf3661901a09a07abc77dee8735d6f8727
Philip Goforth <77207179+DrDiscoChickenShack@users.noreply.github.com>
2021-11-08 13:11:29 -0600

    final

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What temp in Farenheight to convert:");
+            double temp = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"\n{temp} degrees Farenheight is {TempConverter.FarenheightToCelcius(temp)} degrees Celcius");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("What temp in Celcius would you like to convert:");
+            temp = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"\n{temp} degrees Celcius is {TempConverter.CelciusToFarenheight(temp)} degrees Farenheight.");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    class TempConverter
+    {
+        TempConverter()
+        {
+        }
+        public static double CelciusToFarenheight(double temp)
+        {
+            temp = (temp * 9 / 5) + 32;
+            return temp;
+        }
+        public static double FarenheightToCelcius(double temp)
+        {
+            temp = (temp - 32) * 5 / 9;
+            return temp;
+        }
+    }
+}
